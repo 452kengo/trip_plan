12c7acbd9d6f85bd454b3408aa64d0fa9561e46e
EC2 Default User <ec2-user@ip-172-31-5-128.ap-northeast-1.compute.internal>
2022-08-26 12:23:12 +0000

    question 2-1

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,16 @@
 
 ActiveRecord::Schema.define(version: 2022_08_05_073052) do
 
+  create_table "maps", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.text "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "places", force: :cascade do |t|
     t.string "name"
     t.string "description"
